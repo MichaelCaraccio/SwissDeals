e372f20d49fcb3dddd35ddb26e74f4119de25e35
krypty <gmarigliano93@gmail.com>
2015-11-15 12:17:08 +0100

    add Qooking.ch provider

--- app/src/main/java/ch/swissdeals/DummyActivity.java
+++ app/src/main/java/ch/swissdeals/DummyActivity.java
@@ -79,6 +79,7 @@ public class DummyActivity extends AppCompatActivity {
             providerManager.subscribe("digitec.ch");
             providerManager.subscribe("QWine.ch");
             providerManager.subscribe("QSport.ch");
+            providerManager.subscribe("Qooking.ch");
 //            providerManager.subscribe("QoQa.ch");
 //            providerManager.subscribe("microspotYOLO.ch");
 //            providerManager.subscribe("deindeal.ch");
