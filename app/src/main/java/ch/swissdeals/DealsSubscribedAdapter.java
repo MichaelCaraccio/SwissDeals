2119db16db5feef55450f7095a791eed47a77f6f
MichaelCaraccio <michael.caraccio@gmail.com>
2015-12-18 20:10:51 +0100

    Fix price and crossout problem in listview

--- app/src/main/java/ch/swissdeals/DealsSubscribedAdapter.java
+++ app/src/main/java/ch/swissdeals/DealsSubscribedAdapter.java
@@ -21,6 +21,9 @@ import ch.swissdeals.database.models.ModelDeals;
 import ch.swissdeals.database.models.ModelProviders;
 
 public class DealsSubscribedAdapter extends ArrayAdapter<ModelDeals> {
+
+    public static final int CROSS_OUT_COLOR = Color.parseColor("#FF4640");
+
     private final Context context;
     private final List<ModelDeals> values;
     private final DatabaseHelper helper;
@@ -74,12 +77,12 @@ public class DealsSubscribedAdapter extends ArrayAdapter<ModelDeals> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        ViewHolder mViewHolder;
+        ViewHolder viewHolder;
         ModelDeals deal = values.get(position);
         ModelProviders provider = helper.getProvider(deal.getFk_provider_id());
 
-        if(convertView == null){
-            mViewHolder = new ViewHolder();
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
 
             LayoutInflater inflater = (LayoutInflater) context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -89,56 +92,63 @@ public class DealsSubscribedAdapter extends ArrayAdapter<ModelDeals> {
             setCrossOutThickness(5);
 
             convertView = inflater.inflate(R.layout.item_deal, parent, false);
-            mViewHolder.dealTitle = (TextView) convertView.findViewById(R.id.item_title);
-            mViewHolder.dealProviderName = (TextView) convertView.findViewById(R.id.item_providerName);
-            mViewHolder.dealDescription = (TextView) convertView.findViewById(R.id.item_shortDescription);
-            mViewHolder.dealCurrentPrice = (TextView) convertView.findViewById(R.id.item_currentPrice);
-            mViewHolder.dealOldPrice = (TextView) convertView.findViewById(R.id.item_oldPrice);
-            mViewHolder.dealImage = (ImageView) convertView.findViewById(R.id.item_image);
-            mViewHolder.dealFavicon = (ImageView) convertView.findViewById(R.id.item_favicon);
-            mViewHolder.crossOut = (ImageView) convertView.findViewById(R.id.crossOut);
+            viewHolder.dealTitle = (TextView) convertView.findViewById(R.id.item_title);
+            viewHolder.dealProviderName = (TextView) convertView.findViewById(R.id.item_providerName);
+            viewHolder.dealDescription = (TextView) convertView.findViewById(R.id.item_shortDescription);
+            viewHolder.dealCurrentPrice = (TextView) convertView.findViewById(R.id.item_currentPrice);
+            viewHolder.dealOldPrice = (TextView) convertView.findViewById(R.id.item_oldPrice);
+            viewHolder.dealImage = (ImageView) convertView.findViewById(R.id.item_image);
+            viewHolder.dealFavicon = (ImageView) convertView.findViewById(R.id.item_favicon);
+            viewHolder.crossOut = (ImageView) convertView.findViewById(R.id.crossOut);
 
-            if (deal.getOld_price() != -1.0) {
-                crossOutOldPrice(mViewHolder,getBitmapWidth(), 25, 0, getBitmapHeight() - 25, Color.parseColor("#FF4640"));
-            }
-
-            convertView.setTag(mViewHolder);
-
-        }else {
-            mViewHolder = (ViewHolder) convertView.getTag();
+            convertView.setTag(viewHolder);
         }
 
+        viewHolder = (ViewHolder) convertView.getTag();
+
         // TODO : insert null with image placeholder
-        Picasso.with(context).load(provider != null ? provider.getFavicon_url() : null).resize(40, 40).into(mViewHolder.dealFavicon);
+        Picasso.with(context).load(provider != null ? provider.getFavicon_url() : null).resize(40, 40).into(viewHolder.dealFavicon);
 
         // TODO : insert image placeholder instead of null
         Picasso.with(context)
                 .load(deal.getImage_url())
                 .resize(300, 300)
                 .centerCrop()
-                .into(mViewHolder.dealImage);
+                .into(viewHolder.dealImage);
 
-        mViewHolder.dealTitle.setText(deal.getTitle().toUpperCase());
-        mViewHolder.dealProviderName.setText(helper.getProviderNameFromID(deal.getFk_provider_id()).toUpperCase());
-        mViewHolder.dealCurrentPrice.setText(String.format("%.2f", deal.getPrice()));
+        viewHolder.dealTitle.setText(deal.getTitle().toUpperCase());
+        viewHolder.dealProviderName.setText(helper.getProviderNameFromID(deal.getFk_provider_id()).toUpperCase());
+        viewHolder.dealCurrentPrice.setText(String.format("%.2f", deal.getPrice()));
 
-        if (deal.getOld_price() != -1.0)
-            mViewHolder.dealOldPrice.setText(String.format("%.2f", deal.getOld_price()));
+        // If price not equals to -1 draw bitmap and display price
+        if (deal.getOld_price() != -1.0) {
+            crossOutOldPrice(viewHolder, getBitmapWidth(), 25, 0, getBitmapHeight() - 25, CROSS_OUT_COLOR);
+            viewHolder.dealOldPrice.setText(String.format("%.2f", deal.getOld_price()));
+        } else {
+            drawBlankBitmap(viewHolder);
+            viewHolder.dealOldPrice.setText("");
+        }
 
         if (deal.getDescription() != null)
-            mViewHolder.dealDescription.setText(deal.getDescription().toUpperCase());
+            viewHolder.dealDescription.setText(deal.getDescription().toUpperCase());
 
         if (deal.getPrice() == -1.0)
-            mViewHolder.dealCurrentPrice.setText("");
+            viewHolder.dealCurrentPrice.setText("");
 
         return convertView;
+    }
+
+
+    private void drawBlankBitmap(ViewHolder mViewHolder) {
+        Bitmap bmp = Bitmap.createBitmap(getBitmapWidth(), getBitmapHeight(), Bitmap.Config.ARGB_8888);
+        mViewHolder.crossOut.setImageBitmap(bmp);
     }
 
     private void crossOutOldPrice(ViewHolder mViewHolder, float x, float y, float xend, float yend, int color) {
 
         Bitmap bmp = Bitmap.createBitmap(getBitmapWidth(), getBitmapHeight(), Bitmap.Config.ARGB_8888);
+        mViewHolder.crossOut.setImageBitmap(bmp);
         Canvas c = new Canvas(bmp);
-        mViewHolder.crossOut.draw(c);
 
         Paint p = new Paint();
         p.setColor(color);
@@ -147,4 +157,3 @@ public class DealsSubscribedAdapter extends ArrayAdapter<ModelDeals> {
         mViewHolder.crossOut.setImageBitmap(bmp);
     }
 }
-
